3e6eaaaed3001ca49c448708f5752d91a5051346
Your Name <you@example.com>
2026-05-14 00:13:18 +0000

    new try

--- graph.c
+++ graph.c
@@ -50,6 +50,18 @@ void addNode(Graph* g, const char* label) {
 
 void addEdge(Graph* g, const char* src, const char* dest, int weight) {
     if (!g || !src || !dest) return;
+    List *srcEdges = (List*)map_search(g->adjacencyMap, (void*)src);
+    if(srcEdges == NULL) return;
+    Edge *nuevoEdge = (Edge*)malloc(sizeof(Edge));
+    if(nuevoEdge == NULL) return;
+    nuevoEdge->target = (char*)malloc(strlen(dest) + 1);
+    if(nuevoEdge->target == NULL){
+        free(nuevoEdge);
+        return;
+    }
+    strcpy(nuevoEdge->target, dest);
+    nuevoEdge->weight = weight;
+    list_pushBack(srcEdges, nuevoEdge);
 
 }
 
